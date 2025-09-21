990b9ec4d56cdc9390c5e679b78fd185855c01df
duncan <duncand9199@gmail.com>
2025-09-21 00:13:04 -0400

    Finished 2-65

--- code/data/2-65.c
+++ code/data/2-65.c
@@ -24,9 +24,65 @@
 #include <stdio.h>
 #include <assert.h>
 
+/*
+ * "XOR folding"
+ * Can use XOR operations to "fold" down halves of the word until only 
+ * 1 bit remains, e.g., x ^= x >> 16.
+ *
+ * So naturally, two numbers XOR'd together = 1 if the number of 1s in both
+ * is odd.
+*/
+
+/*
+ * Sample value of x = 0x12345678
+ * 0x12345678 = 0b 0001 0010 0011 0100 0101 0110 0111 1000
+ * b0 (LSB), b31 (MSB)
+ * low 16 = 0x5678 (bits b0 - b15)
+ * high 16 = 0x1234 (b16 - b31)
+ *
+ * First operation...
+ * After x ^= x >> 16,
+ * x >> 16 = 0x00001234
+ * x = 0x12345678 ^ 0x00001234 = 0x1234444C = 0b 0001 0010 0011 0100 0100 0100 0100 1100
+ * Upper half bits 16 - 31 remain equal to their original values in this step. 
+ * LSB = 0
+ *
+ * Second...
+ * After x ^= x >> 8,
+ * x >> 8 = 0x00123444 (using current x)
+ * x = 0x1234444C ^ 0x00123444 = 0x12267008 = 0b 0001 0010 0010 0110 0111 0000 0000 1000
+ * LSB = 0
+ *
+ * Third...
+ * After x ^= x >> 4,
+ * x = 0x12267008 ^ (0x12267008 >> 4) = 0x13041708 = 0b 0001 0011 0000 0100 0001 0111 0000 1000
+ * LSB = 0
+ *
+ * Fourth...
+ * After x ^= x >> 2,
+ * x = 0x13041708 ^ (0x13041708 >> 2) = 0x17C512CA = 0b0001 0111 1100 0101 0001 0010 1100 1010
+ * LSB = 0
+ *
+ * Fifth...
+ * After x ^= x >> 1,
+ * x = 0x17C512CA ^ (0x17C512CA >> 1) = 0x1C279BAF = 0b 0001 1100 0010 0111 1001 1011 1010 1111
+ * LSB = 1
+ *
+ * Finally, x & 1 = 1. Parity is odd.
+ *
+ * "Folding" like this doubles the group of original bit positions whose XOR is stored in the 
+ * lower indices. After the last >> 1 step, bit 0 holds the XOR of EVERY original bit 
+ * in the 32-bit word.
+*/
 int odd_ones(unsigned x) {
-	// Need to finish this. Can't for the life of me figure it out
-	return x;
+	
+	x ^= x >> 16;	// fold upper 16 bits into lower 16
+	x ^= x >> 8;	// fold upper 8 bits into lower 8
+	x ^= x >> 4;	// and so on...
+	x ^= x >> 2;	
+	x ^= x >> 1;
+	
+	return x & 1;	// extract parity bit
 }
 
 int main(void) {
