bebb9c038ad2bb7f2d722afbadae222784341d50
duncan <duncand9199@gmail.com>
2025-09-25 20:37:47 -0400

    Finished 2-68.c

--- code/data/2-68.c
+++ code/data/2-68.c
@@ -20,4 +20,31 @@
  * Be careful of the test case n = w.
 */
 
-int lower_one_mask(int n);
+#include <stdio.h>
+#include <assert.h>
+
+int lower_one_mask(int n) {
+	int w = sizeof(int) << 3;	// number of bits in int
+
+	/* Create an unsigned value with all bits = 0
+	 * Right shift by (w - n).
+	 * For 1 <= n <= w, (w - n) is in [0, w-1], so this 
+	 * is well defined. The result is an unsigned value whose low n 
+	 * bits are 1.
+	*/	
+	unsigned u = ~0u;
+	return (int)(u >> (w - n));
+}
+
+int main(void) {
+	int w = sizeof(int) << 3;
+
+	assert(lower_one_mask(1) == 0x1);
+	assert(lower_one_mask(6) == 0x3F);
+	assert(lower_one_mask(8) == 0xFF);
+	assert(lower_one_mask(17) == 0x1FFFF);	
+	assert(lower_one_mask(w) == ~0);	// all ones
+	
+	printf("All tests passed.\n");
+	return 0;
+}
