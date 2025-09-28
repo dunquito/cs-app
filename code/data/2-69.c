8424d80edaf3466d24f3af46b468828ed2058f3e
duncan <duncand9199@gmail.com>
2025-09-27 21:17:02 -0400

    Finished 2-69.c

--- code/data/2-69.c
+++ code/data/2-69.c
@@ -20,48 +20,76 @@
  * Be careful of the case n = 0.
 */
 
+#include <stdio.h>
+#include <assert.h>
+
 /*
- * If n = 4
- * We're right shifting by w - n
- * 32 - 4 = 28
+ * Originally, I had something that simplified down to something like:
  *
- * Say, 11110000 00000000 00000000 00000000
- * then 00000000 00000000 00000000 00001111
+ * rotate_left(unsigned x, n) = (x << n) | ((unsigned)x >> (w - n))
  *
- * But we also need to shift the remaining bits LEFT by an equal amount
- * So create two variables out of x
+ * However, after failing some of my own tests I realized that 
+ * this expression is undefined when n == 0, because it makes you 
+ * shift by w (i.e., w - n == w). 
  *
- * Right shift one by (w - n)
- * Left shift the other by n
- *
- * Then... OR them together? We need the right shift to be LOGICAL...
- *
- * x = 11110101 01010101 01010101 01010101
- * n = 4
- * The result we're looking for is
- * 01010101 01010101 01010101 01011111
- *
- * So
- * this = x >> (w - n) -> x >> (32 - 4) -> x >> 28
- * this = 00000000 00000000 00000000 00001111
- *
- * And
- * that = x << 4
- * that = 01010101 01010101 01010101 01010000
- *
- * return this | that
+ * To avoid this (without using conditionals), I had to mask the second 
+ * shift amount so that when n == 0, the shift amount becomes 0 (which is 
+ * safe), and when n != 0 the shift amount is w - n (also safe, within the 
+ * range 0..w-1).
 */
 unsigned rotate_left(unsigned x, int n) {
-	int w = sizeof(int) << 3;
+	int w = sizeof(int) << 3; 			// number of bits in int (w >= 1)
 
-	unsigned int right = x >> (w - n);
-	unsigned int left = x << n;
+	// left: shift x by n (safe because 0 <= n < w)
+	unsigned left = x << n;
 
-	return right | left;
+	/*
+	 * Computing shift amount for the right part safely, even when n == 0
+	 *
+	 * So amt becomes = (w - n) & (-!!n)
+	 *
+	 * !!n -> 0 when n == 0, 1 otherwise
+	 * -!!n -> 0 when n == 0, -1 otherwise
+	 *  
+	 *  Then, casting to unsigned gives 0u, or just a mask of all ones
+	 *  (w - n) & mask gives 0 when n == 0 (so we're just shifting by 0), else w - n when n != 0
+	 *
+	 *  This prevents shifting by w, which would be undefined
+	*/
+	unsigned mask = (unsigned)(-!!n);		// 0u when n == 0, else 0xFFFFFFFF ...
+	unsigned amt = ((unsigned)(w - n)) & mask;	// 0 when n == 0, else w - n
+	
+	unsigned right = x >> amt;
+
+	return (int)(left | right);
 }
 
-// need to finish and run tests
+// Testing
 int main(void) {
+	int w = sizeof(int) << 3;
+
+	// Given examples
+	assert(rotate_left(0x12345678, 4) == 0x23456781);
+	assert(rotate_left(0x12345678, 20) == 0x67812345);
+
+	// n == 0 should be identity
+	assert(rotate_left(0x12345678, 0) == 0x12345678);
+
+	// rotate by 1 and by w - 1
+	assert(rotate_left(0x80000000, 1) == 0x00000001); // top bit moves to low bit
+	assert(rotate_left(0x00000001, w - 1) == 0x80000000); // same as above
+	
+	// some random checks
+	for (int n = 0; n < w; ++n) {
+		unsigned x = 0xDEADBEEFu;
+		unsigned ref;
+
+		if (n == 0) ref = x;
+		else ref = (x << n) | (x >> (w - n));
+		assert(rotate_left(x, n) == ref);
+	}
+
+	printf("All tests passed.\n");
 	return 0;
 }
 	
