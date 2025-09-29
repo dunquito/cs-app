7238f13aab0db4e2b3c01bfca6535a8ec0d9e0ec
duncan <duncand9199@gmail.com>
2025-09-28 21:05:38 -0400

    Started 2.70.c

---
+++ code/data/2.70.c
@@ -0,0 +1,30 @@
+/*
+ * 2.70
+ *
+ * Write code for the function with the following prototype:
+ *
+ * Return 1 when x can be represented as an n-bit, 2's-complement number;
+ * 0 otherwise
+ * Assume 1 <= n <= w
+ *
+ * Code must follow the same integer coding rules:
+ *
+ * 1. Integers are represented in two's complement form
+ * 2. Right shifts of signed data are performed arithmetically
+ * 3. Data type int is "w" bits long. Can use sizeof(int) << 3 to compute w.
+ * 4. Conditionals, loops, switch statements, function calls, and macro
+ * invocations are forbidden,
+ * 5. Division, modulus, and multiplication are forbidden.
+ * 6. Relative comparison operators (<, >, <=, >=) are forbidden.
+ *
+*/
+
+// Still need to implement...
+int fits_bits(int x, int n) {
+	return 0;
+}
+
+// For testing... later
+int main(void) {
+	return 0;
+}
